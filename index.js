631bc5fb8c4a0313f1ddb0ba7c1206cb2d7ad661
braedonb98 <brbe3574@colorado.edu>
2021-11-08 11:15:53 -0700

    adding files to git

---
+++ index.js
@@ -0,0 +1,88 @@
+
+//-------------------------------------------------------------------------https-------------------------------------------------------------------------------------------------
+// content of index.js
+//const http = require('http')
+/*const port = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})*/
+
+//------------------------------------------------------------------------------express test v1---------------------------------------------------------------------------------------
+/*
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+  response.send('Hello from Express!')
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})*/
+
+
+
+//------------------------------------------------------------------------------------------Express v2
+/*
+const express = require('express')
+const app = express()
+
+app.use((request, response, next) => {
+  console.log(request.headers)
+  next()
+})
+
+app.use((request, response, next) => {
+  request.chance = Math.random()
+  next()
+})
+
+app.get('/', (request, response) => {
+  response.json({
+    chance: request.chance
+  })
+})
+
+app.listen(3000)*/
+
+
+
+
+// index.js
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+console.log("here");
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'John'
+  })
+})
+app.listen(3000)
